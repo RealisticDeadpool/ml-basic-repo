9cabcb59ea73bc6ced1a9771fad3da6013c7bc6d
Chen_Fuqiang_12138 <chanfuqiang@hotmail.com>
2021-03-22 21:33:49 +0800

    change the iditor from pycharm to jupyter.

--- small-practise/JunZhiYiChu.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import sklearn.preprocessing as sp
-raw_samples = np.array([
-    [17., 100., 4000],
-    [20., 80., 5000],
-    [23., 75., 5500]])
-
-std_samples = sp.scale(raw_samples)
-print(std_samples)
